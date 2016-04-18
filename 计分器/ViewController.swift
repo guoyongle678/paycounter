0844b323e5a8a1919dc1208b8e9461c9bd1c6eae
guoyongle678 <lele@mac04.local>
2016-04-18 21:09:36 +0800

    1.1

--- 计分器/ViewController.swift
+++ 计分器/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Ryn here")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
