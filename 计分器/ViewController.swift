3823e5131634fab51810b7354c0ff3d9353d551a
guoyongle678 <lele@mac04.local>
2016-04-18 21:02:59 +0800

    界面1.0
    1.0

---
+++ 计分器/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  计分器
+//
+//  Created by 乐乐 on 16/4/18.
+//  Copyright © 2016年 乐乐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
