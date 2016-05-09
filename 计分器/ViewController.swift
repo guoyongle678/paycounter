fc60f4995acd4d512375e22c53f6056797a5eaed
guoyongle678 <lele@mac04.local>
2016-05-09 21:05:10 +0800

    1.7甲乙队得分
    1.7甲乙队得分

--- 计分器/ViewController.swift
+++ 计分器/ViewController.swift
@@ -10,23 +10,89 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var jiaduidefen: UILabel!
-    var operand1: String = ""
     var x:Int = 0;
-        @IBAction func jiayy(sender: UIButton) {
-            x++
-            jiaduidefen.text = "\(x)";
-            
-            
+    @IBAction func jiayy(sender: UIButton) {
+        x++
+        jiaduidefen.text = "\(x)";
     }
-    override func viewDidLoad() {
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func jiajia2(sender: UIButton) {
+        x=x+2
+        jiaduidefen.text = "\(x)";
+    }
+    @IBAction func jiajia3(sender: UIButton) {
+        x=x+3
+        jiaduidefen.text = "\(x)";
     }
 
+ 
+    @IBOutlet var yiduidefen: UILabel!
+    var y:Int = 0;
+    @IBAction func yidui1(sender: UIButton) {
+        y=y+1
+        yiduidefen.text = "\(y)";
+    }
+    @IBAction func yidui2(sender: UIButton) {
+        y=y+2
+        yiduidefen.text = "\(y)";
+    }
+    @IBAction func yidui3(sender: UIButton) {
+        y=y+3
+        yiduidefen.text = "\(y)";
+    }
+    
+    
+    
+    
+    
+    
+    
+    var db:SQLiteDB!
+    
+    @IBOutlet var txtUname: UITextField!
+    @IBOutlet var txtMobile: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //获取数据库实例
+        db = SQLiteDB.sharedInstance()
+        //如果表还不存在则创建表（其中uid为自增主键）
+        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        //如果有数据则加载
+        initUser()
+    }
+    
+    //点击保存
+    @IBAction func saveClicked(sender: AnyObject) {
+        saveUser()
+    }
+    
+    //从SQLite加载数据
+    func initUser() {
+        let data = db.query("select * from t_user")
+        if data.count > 0 {
+            //获取最后一行数据显示
+            let user = data[data.count - 1]
+            txtUname.text = user["uname"] as? String
+            txtMobile.text = user["mobile"] as? String
+        }
+    }
+    
+    //保存数据到SQLite
+    func saveUser() {
+        let uname = self.txtUname.text!
+        let mobile = self.txtMobile.text!
+        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
+        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        print("sql: \(sql)")
+        //通过封装的方法执行sql
+        let result = db.execute(sql)
+        print(result)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
-
+}
